3688c3402b795c83c394373e7f69727295e70df9
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2024-01-20 18:40:46 +0100

    analyse_trame_hex.py

--- analyse_trame_hex.py
+++ analyse_trame_hex.py
@@ -1,13 +1,14 @@
 #affichage des trames en hexa
-def print_bytes(data):  
-    text = ""
-    for i in range(0, len(data)):
-        text = text + f"{data[i]:02x}"
-    print(text)
+def conversion_trame_bytes(trame):  
+    trame_hex = ""
+    for i in range(0, len(trame)):
+        trame_hex = trame_hex + f"{trame[i]:02x}"
+    return trame_hex
     
-# hex: f1 0d 00 02 aa 02 49 00 00 49 00 31 37 00 55 
+# f1 0d 00 50 32 aa 02 20 00 49 00 31 37 00 55 
 if __name__ == "__main__":
     trame = b'\xf1\r\x0d\x502\xaa\x02\x20\x00I\x0017\x00U'
+    resultat = conversion_trame_bytes(trame)   
     print(trame)
-    print_bytes(trame)
+    print(resultat)
 
